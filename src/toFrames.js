01e222cc27b6d89447b734ccd41e4748136b4447
Matas Jonys <matasjonys@gmail.com>
2017-08-19 02:30:23 +0300

    Naming fix

--- src/toFrames.js
+++ src/toFrames.js
@@ -10,10 +10,10 @@ const timecodeToFloat = (timecode) => {
   const hours = timeArray[0] * hourInSeconds;
   const minutes = parseInt(timeArray[1] * minuteInSeconds, 10);
   const seconds = parseInt(timeArray[2], 10);
-  const millis = parseInt(timeArray[3], 10);
-  const frame = millis / framesPerSecond;
+  const frame = parseInt(timeArray[3], 10);
+  const millis = frame / framesPerSecond;
 
-  return parseFloat(hours + minutes + seconds + frame);
+  return parseFloat(hours + minutes + seconds + millis);
 };
 
 export default (timecode) => {
